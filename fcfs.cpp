fe9504bcc36f6c3c3f71ccf66fefc20c729a713b
Anshul <135310216+Ashus25@users.noreply.github.com>
2024-03-27 00:22:10 +0530

    Add files via upload

--- fcfs.cpp
+++ fcfs.cpp
@@ -11,8 +11,47 @@ struct Process
   int waiting_time;
 };
 
-void findCompletionTime(Process p[],)
+void findCompletionTime(Process p[], int n)
 {
+  p[0].completion_time = p[0].burst_time;
+  for (int i = 1; i < n; i++)
+  {
+    p[i].completion_time = p[i - 1].completion_time + p[i].burst_time;
+  }
+}
+
+void findTurnAroundTime(Process p[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    p[i].turn_around_time = p[i].completion_time - p[i].arrival_time;
+  }
+}
+
+void findWaitingTime(Process p[], int n)
+{
+  p[0].waiting_time = 0;
+  for (int i = 1; i < n; i++)
+  {
+    p[i].waiting_time = p[i].turn_around_time - p[i].burst_time;
+  }
+}
+
+void findFCFS(Process p[], int n)
+{
+  findCompletionTime(p, n);
+  findTurnAroundTime(p, n);
+  findWaitingTime(p, n);
+}
+
+void printFCFS(Process p[], int n)
+{
+  cout << "FCFS Scheduling :" << endl;
+  cout << "PID\tArrival Time\tBurst Time\t Completion Time\tTurnaround Time\t\tWaiting Time" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << p[i].pid << "\t\t" << p[i].arrival_time << "\t\t" << p[i].burst_time << "\t\t" << p[i].completion_time << "\t\t" << p[i].turn_around_time << "\t\t\t" << p[i].waiting_time << endl;
+  }
 }
 
 int main()
@@ -39,6 +78,8 @@ int main()
   // for (int i = 0; i < n; i++) {
   //     cout << "Process " << p[i].pid << ": Arrival Time = " << p[i].arrival_time << ", Burst Time = " << p[i].burst_time << endl;
   // }
+  findFCFS(p, n);
+  printFCFS(p, n);
 
   return 0;
 }
